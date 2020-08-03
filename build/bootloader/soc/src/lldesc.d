f1ce34e65a77a15af82595f777d176d5fd4924fa
Jody L. Whitlock <jody.whitlock@tiberiansun.us>
2020-08-02 22:14:49 -0500

    First Build

--- build/bootloader/soc/src/lldesc.d
+++
@@ -1,8 +0,0 @@
-src/lldesc.o: /home/jody/esp/esp-idf/components/soc/src/lldesc.c \
- /home/jody/esp/esp-idf/components/soc/include/soc/lldesc.h \
- /home/jody/esp/esp-idf/components/esp_rom/include/esp32/rom/lldesc.h
-
-/home/jody/esp/esp-idf/components/soc/include/soc/lldesc.h:
-
-/home/jody/esp/esp-idf/components/esp_rom/include/esp32/rom/lldesc.h:
-/home/jody/esp/esp-idf/components/soc/src/lldesc.c:
--- build/esp_common/src/dbg_stubs.d
+++ build/esp_common/src/dbg_stubs.d
@@ -6,10 +6,7 @@ src/dbg_stubs.o: \
  /home/jody/esp/esp-idf/components/esp_common/include/esp_err.h \
  /home/jody/esp/esp-idf/components/newlib/platform_include/assert.h \
  /home/jody/esp/myprojects/esp32-ota-example/build/include/sdkconfig.h \
- /home/jody/esp/esp-idf/components/esp32/include/esp_attr.h \
- /home/jody/esp/esp-idf/components/log/include/esp_log.h \
- /home/jody/esp/esp-idf/components/esp_rom/include/esp32/rom/ets_sys.h \
- /home/jody/esp/esp-idf/components/log/include/esp_log_internal.h
+ /home/jody/esp/esp-idf/components/esp32/include/esp_attr.h
 
 /home/jody/esp/esp-idf/components/xtensa/include/eri.h:
 
@@ -24,10 +21,4 @@ src/dbg_stubs.o: \
 /home/jody/esp/myprojects/esp32-ota-example/build/include/sdkconfig.h:
 
 /home/jody/esp/esp-idf/components/esp32/include/esp_attr.h:
-
-/home/jody/esp/esp-idf/components/log/include/esp_log.h:
-
-/home/jody/esp/esp-idf/components/esp_rom/include/esp32/rom/ets_sys.h:
-
-/home/jody/esp/esp-idf/components/log/include/esp_log_internal.h:
 /home/jody/esp/esp-idf/components/esp_common/src/dbg_stubs.c:
