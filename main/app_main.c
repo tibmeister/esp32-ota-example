aae9c83d0fc228a8115a3d18d6303183500b644b
Jody L. Whitlock <jody.whitlock@tiberiansun.us>
2020-08-04 00:25:51 -0500

    Removed extra restart

--- main/app_main.c
+++ main/app_main.c
@@ -100,7 +100,6 @@ void app_main()
     {
         ESP_LOGI(TAG, "Update was successful, switching to run this going forward");
         esp_err_t errRb = esp_ota_mark_app_valid_cancel_rollback();
-        esp_restart();
     }
     else
     {
